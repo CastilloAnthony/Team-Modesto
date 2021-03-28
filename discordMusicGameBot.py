a5975d31eee02c9e2b7835f9918e8248fd1685ff
Anthony Castillo <anthony1093ca@gmail.com>
2021-03-27 19:55:44 -0700

    All we have

--- discordMusicGameBot.py
+++ discordMusicGameBot.py
@@ -1,6 +1,12 @@
-import spotipy
+import discord
+from discord.ext import commands
+
+import os
+import random
+import json
 import matplotlib
-from spotipy.oauth2 import SpotifyClientCredentials
+
+#arrays start at 0
 
 #access token dictionaries
 discordAccessTokens = {
@@ -14,7 +20,7 @@ discordAccessTokens = {
 
 spotifyAccessTokens = {
     'client_id': '88537321562449afb8875bf3d823e046',
-    'client_secret': '4d306a0ee0114538953b7aa58c3c91f1',
+    'client_secret': 'secret',
   } 
 
 geniusAccessTokens = {
@@ -30,19 +36,42 @@ geniusAccessTokens = {
 
 url = "" #discord/spotify/genius
 api = "" #discord/spotify/genius
+
 playerScoreData = {
     'playerID' : score, #either their discord ID or just an abbreviated name. or perhaps their nickname
     'playerID' : score,
     'playerID' : score,
     'playerID' : score,
-    #and so on
+    } #and so on and on and on
+
+playerMessageDataQueue = {
+    'playerID' : message,
     }
 
-#function prototypes
+listOfBotCommands = {
+    'command1' : "!play gametype 1",
+    'command2' : "!play gametype 2",
+    'command3' : "!pause",
+    'command4' : "!resume"
+    }
+
+discordTextChannel = "music" #or something else
+
+#function prototypes below
+
+def startGame(int): #kind of like our main()
+    return #starts the requested game type
 
 def apiHandshakes(api):
     #code here
     return #ture/false # if it successfully connects to either the specified api or we could try to connect to all at once
+
+def connectToDiscordChannels(dictionary): 
+    #tries to connect to one (or both channels depending on the gamemode) the bot's channel
+    return #true/false succuess/fail
+
+def getPlayers():
+    return #a dictionary(or another data structure) of all the participating players' discord names
 
 def getWebData(url):
     return #should import and return a (dictionary, .json, or other data structure) from the website api 
@@ -50,33 +79,59 @@ def getWebData(url):
 def randomizePlaylist():
     return #should return a randomized (dictionary, .json, or other data structure) of songs based on the imported one 
 
-def connectToChannels(dictionary): 
-    #tries to connect to one (or both channels depending on the gamemode) the bot's channel
-    return #true/false succuess/fail
+def readSpecifiedTextChannel(discordTextChannel):
+    #input the playerID and the contents of that message into the playerMessageDataQueue dictionary
+    return #maybe return the ammended dictionary (or other data structure)? 
 
-def readSpecifiedTextChannel():
-    return #returns a string (or other data structure) of the name of the player who sent the message and the contents of that message
-
-def command(string):
-    #start a game, change a rule, whitelist a player(?), display a list of !commands in the designated 
+def issueCommand(message):
+    #change a rule
+    #whitelist a player(?)
+    #display a listOfBotCommands in the designated channel
+    #!add playlist
+    #!start gametype 1/2/3/4/???
+    #!help
+    #!players
+    #!scoreboard
+    #anything else
     return #true/false if the command was enacted successfully
 
-def startGame(int):
-    return #starts the requested game type
-
-def getPlayers():
-    return #a dictionary(or another data structure) of all the participating players' discord names
-
+def calculateScore(message):
+    #we need to calculate their score
+    #do they get partial credit?
+    return #return the value of the score based on the inputted message
 
 
+@commands.command()
+async def create(self, ctx):
+    """this is to create a json file"""
+    user=str(ctx.author)
+    id=str(ctx.author.id)
+    with open('players.json') as f:
+        users=json.load(f)
+    if not id in users:
+        await create(users, user, id)
+        await ctx.send('player created')
+        with open('players.json', 'w') as f:
+            json.dump(users, f)
 
-
+@commands.command()
+async def get(self, ctx):
+    with open('players.json') as f:
+        users=json.load(f)
+    await ctx.send(user[str(ctx.author.id)])
+    
+def create(players, user, id):
+    players[id] = {}
+    players[id]['name'] = user
+    players[id]['score'] = 0 #inital score
 
 
 
 #the code below is copied from https://github.com/plamere/spotipy
+from spotipy.oauth2 import SpotifyClientCredentials
 sp = spotipy.Spotify(auth_manager=SpotifyClientCredentials(spotifyAccessTokens[client_id], spotifyAccessTokens[client_secret]))
 
-#results = sp.search(q='weezer', limit=20)
-#for idx, track in enumerate(results['tracks']['items']):
-#    print(idx, track['name'])
+query = "" #this should be all the songs from the playlist could be 
+results = sp.search(query, limit=20)
+for idx, track in enumerate(results['tracks']['items']):
+    #print()#idx, track['name']
